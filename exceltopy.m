251dcc6ba73d5ca6c27c19896baf9fed91372bbb
edgarsanp <edgarsanp@hotmail.com>
2017-05-29 21:07:24 +0200

    Add files via upload

--- exceltopy.m
+++ exceltopy.m
@@ -18,8 +18,8 @@ fprintf(fid,'%s\r\n','');
 
 %%%%%%%%% DAUG_PARENT LIST %%%%%%%%%%%%%%%%%
 
-strDict_1 = 'daug_parent = {';
-str_aux = strcat('''', txt(1,1),'''', ':', '''', txt(1,2), '''');
+strDict_1 = 'daug_parent = {\n';
+str_aux = strcat('\t','''', txt(1,1),'''', ':', '''', txt(1,2), '''');
 strDict_1 = strjoin([strDict_1, str_aux]);
 
 for i=2:Ndades
@@ -27,12 +27,12 @@ for i=2:Ndades
     % txt(i,2) es correspon a la categoria
     % txt(i,3) es correspon a l'atom
     
-    str_aux = [', ''', txt(i,1), '''', ':', '''', txt(i,2), ''''];
+    str_aux = [',\n\t''', txt(i,1), '''', ':', '''', txt(i,2), ''''];
     strDict_1 = strjoin([strDict_1, str_aux]);
 
 end 
 
-strDict_1 = [strDict_1, '}'];
+strDict_1 = [strDict_1, '\n}'];
 fprintf(fid,'%s\r\n',strDict_1);
 
 fprintf(fid,'%s\r\n','');
@@ -41,11 +41,11 @@ fprintf(fid,'%s\r\n','');
 
 % Get unique values of parents and atoms
 
-parentatom = txt(1:13,4:5);
+parentatom = txt(1:17,4:5);
 Ndades2 = length(parentatom);
 
-strDict_2 = 'parent_atom = {';
-str_aux = strcat('''', parentatom(1,1), '''', ':', '''', parentatom(1,2), '''');
+strDict_2 = 'parent_atom = {\n';
+str_aux = strcat('\t','''', parentatom(1,1), '''', ':', '''', parentatom(1,2), '''');
 strDict_2 = strjoin([strDict_2, str_aux]);
 
 for i=2:Ndades2
@@ -53,12 +53,12 @@ for i=2:Ndades2
     % txt(i,2) es correspon a la categoria
     % txt(i,3) es correspon a l'atom
     
-    str_aux = [', ''', parentatom(i,1), '''', ':', '''', parentatom(i,2), ''''];
+    str_aux = [',\n\t''', parentatom(i,1), '''', ':', '''', parentatom(i,2), ''''];
     strDict_2 = strjoin([strDict_2, str_aux]);
 
 end 
 
-strDict_2 = [strDict_2, '}'];
+strDict_2 = [strDict_2, '\n}'];
 fprintf(fid,'%s\r\n',strDict_2);
 
 fclose(fid);
